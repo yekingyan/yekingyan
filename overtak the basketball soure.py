e9b78e7eac654e32d5df8bb779538d8e9bdbef5c
yekingyan <529616@gmail.com>
2018-04-26 20:34:40 +0800

    Initial commit

---
+++ overtak the basketball soure.py
@@ -0,0 +1,21 @@
+﻿#-------------------------------------------------------------------------------
+# Name:        overtak the basketball soure
+# Purpose:     Calculat the probability of a basketball score being overtaken
+# Author:      yekingyan
+# Created:     24/04/2018
+#-------------------------------------------------------------------------------
+
+points = int(input('what\'points now?:')) #输入分数
+cut3 = points - 3
+hasball = raw_input('Do the leading  team has ball (yes/no)')#确认控球
+if hasball == "yes":
+    cut3 += 0.5
+else:
+    cut3 -= 0.5
+if cut3 <= 0:
+    cut3 =0
+time = int(raw_input('how much second if left?'))#终场时间
+if cut3 ** 2 > time:
+    print 'safe'
+else:
+    print 'dangerouse'
