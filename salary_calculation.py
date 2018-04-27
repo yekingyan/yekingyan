44513d0956e84a322cb161aafe6388fd31f7cf13
yekingyan <529616@gmail.com>
2018-04-27 19:12:40 +0800

    计算工资日薪与时薪
    时薪高才是工资高

---
+++ salary_calculation.py
@@ -0,0 +1,12 @@
+﻿#-----------
+# Name:        计算工资日薪与时薪
+# Created:     22/04/2018
+#coding:       utf-8
+#------------
+
+msalary = float(raw_input('please input your monthly salary:'))#月工资
+days = float(raw_input('how many days do you work in a month?'))#工作天数
+hours = float(raw_input('how many hours do you work in a day?'))#每天工作时长
+dailypay = msalary / days
+hourlypay = dailypay / hours
+print 'daily pay , hourly pay',dailypay,hourlypay
